9c3effa1bf0d4e56766b5fcee4b019ee3efd09d3
kriptoe <rosswoolf@gmail.com>
2023-08-21 19:53:35 +0800

    z

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,18 +8,16 @@ import lotteryContract from "../contracts/Lottery.json"; // Raw ABI import (pull
 import nftContract from "../contracts/NFT.json"; // Raw ABI import (pulled from etherscan)
 import Navbar from "./Navbar"; // Import the Navbar component
 import styles from "../styles/index.module.css";
-import CountdownTimer2 from "./timer";
 import Image from 'next/image';
 
 
 export default function NumberSelection() {
 
- const FLOOR101_ADDRESS = "0xdC9E96d58903289E2A4771c63fa930d9d56384bA";   // sep polygon  
+  const FLOOR101_ADDRESS = "0xdC9E96d58903289E2A4771c63fa930d9d56384bA";   // sep polygon  
+ // const Lotto_ADDRESS = "0x6c90b24Bb6ca8556C09a1b328388B602d32B7266";  // base goerli
  const Lotto_ADDRESS = "0x55Ff01197C771E1f7f97772aC9860C1F00C5F083";
-
   //const {address, isConnected} = useAccount();
-  const [ethSale, setEthSale] = useState(0);  // cost of NFTs being purchased
-  const [endDate, setEndDate] = useState(0);  // the time/date the lottery ends 
+  const [ethSale, setEthSale] = useState(0);  // cost of NFTs being purchased 
   const [txHash, setTxHash] = useState(0);
    // State to store selected numbers for each game
   const [selectedNumbers, setSelectedNumbers] = useState([]);
@@ -31,11 +29,9 @@ export default function NumberSelection() {
   const [numGames, setNumGames] = useState(1); // State to store the number of games selected
   const [nftPurchased, setNftPurchased] = useState(false); // New state for tracking NFT purchase
 
-  const endDate2 = BigInt(endDate); // Replace this with the number of seconds you want to add
-  const currentDate = new Date();
-  const targetDate = new Date(currentDate.getTime() + Number(endDate2) * 1000);
   const [contractBalance, setContractBalance] = useState(ethers.BigNumber.from(0));
-  const entryFee = 0.1;  // entryFee
+  const entryFee = 0.1;  // entryFee approx 10cents at $1650
+  const entryFeeWEI = ethers.BigNumber.from("100000000000000000"); // 0.0001 ETH in wei
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -51,10 +47,33 @@ export default function NumberSelection() {
     abi: nftContract,
   };
 
+  const { data: getSupply, error: getFError } = useContractRead({
+    ...contractConfigFLOOR,
+    functionName: "totalSupply",
+  });
+
   const { data: getEth, error: lotteryNumberError } = useContractRead({
     ...contractConfig,
     functionName: "s_lotteryNumber",
   });
+
+
+  useEffect(() => {
+    // Define the provider
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+  
+    // Fetch the contract balance
+    const fetchContractBalance = async () => {
+      try {
+        const balance = await provider.getBalance(Lotto_ADDRESS);
+        setContractBalance(balance);
+      } catch (error) {
+        console.error('Error fetching contract balance:', error);
+      }
+    };
+
+    fetchContractBalance();
+  }, [Lotto_ADDRESS]);
 
   useEffect(() => {
     if (getEth) {
@@ -73,7 +92,84 @@ useEffect(() => {
   }
 }, [saleSucceeded]);
 
+function handleNumGamesChange(event) {
+  const selectedValue = parseInt(event.target.value);
+  const maxNumGames = 10; // Maximum number of games
+  const newNumGames = Math.min(selectedValue, maxNumGames);
+  console.log('New numGames value:', newNumGames); // Add this line
+  setNumGames(newNumGames);
+}
 
+useEffect(() => {
+  if (typeof window !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const floor101Contract = new ethers.Contract(FLOOR101_ADDRESS, nftContract, provider);
+
+    const handleMintEvent = async (sender, NFTid) => {
+      console.log("mintEvent:", sender, NFTid.toString());
+      const nftId = NFTid.toString();
+      try {
+        const imageUrl = await fetchTokenURI(nftId, floor101Contract);
+        if (imageUrl) {
+          setNftImageUrl(imageUrl);
+        }
+      } catch (error) {
+        console.log("Error fetching token URI:", error);
+      }
+    };
+
+    // Remove previous event listener
+    floor101Contract.off("mintEvent", handleMintEvent);
+    
+    // Add a new event listener
+    floor101Contract.on("mintEvent", handleMintEvent);
+  }
+}, [saleSucceeded]);
+
+
+
+  const buyLottoTicket = async (numberOfTickets) => {
+    let nftID;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const marketContract = new ethers.Contract(Lotto_ADDRESS, lotteryContract, provider);
+    const marketWithSigner = marketContract.connect(signer);
+  
+    try {
+
+      console.log ("flatten total price" )
+      // Flatten the selectedNumbers array to a 1-dimensional array
+      const flatSelectedNumbers = selectedNumbers.flat();
+      // Calculate the total price based on the number of tickets
+      const totalPrice =entryFeeWEI.mul(numberOfTickets);
+      
+      // Call enterLotteryBULK function with the flattened array and the dynamic price
+      let tx = await marketWithSigner.enterLotteryBULK(flatSelectedNumbers, { value: totalPrice });
+      setSubmitButtonText('Pending');
+      setIsPulsing(true); // Start the pulse animation
+      const receipt = await tx.wait();
+      setTxHash(`https://goerli.basescan.org/tx/${tx.hash}`);
+      // Reset the selected numbers after a successful transaction
+      setSelectedNumbers(Array.from({ length: numGames }, () => []));
+      setNftPurchased(true); // Set nftPurchased to true after a successful transaction
+
+    } catch (e) {
+      if (e.code === "UNPREDICTABLE_GAS_LIMIT") {
+        alert("Sorry, an error occurred while estimating gas. Has the lottery ended?.", e);
+      }
+     // alert(e);
+    } finally {
+      // Reset the submit button text after the transaction completes
+      setSubmitButtonText('Submit');
+      setIsPulsing(false); // Start the pulse animation
+      setSaleSucceeded(true); // Set saleSucceeded to true to trigger the useEffect
+     // Reset numGames to 1 after the successful transaction
+     console.log("resetting")
+      setNumGames(1);
+      setSelectedNumbers(Array.from({ length: numGames }, () => [])); // Reset selectedNumbers array
+    }
+  };
+  
 // Function to handle number selection for each game
 function selectNumber(number, gameIndex) {
   setSelectedNumbers((prevSelectedNumbers) => {
@@ -100,21 +196,45 @@ function selectNumber(number, gameIndex) {
   });
 }
 
-function handleNumGamesChange(event) {
-  const selectedValue = parseInt(event.target.value);
-  const maxNumGames = 10; // Maximum number of games
-  const newNumGames = Math.min(selectedValue, maxNumGames);
-  console.log('New numGames value:', newNumGames); // Add this line
-  setNumGames(newNumGames);
+// This function makes an entry into the lotto draw
+const enterLotto = async () => {
+  const selectedNumbersCount = selectedNumbers.reduce(
+    (total, numbers) => total + (numbers ? numbers.length : 0),
+    0
+  );
+  
+  if (selectedNumbersCount !== numGames * 3) {
+    alert('Please select exactly 3 numbers for each game.');
+    return;
+  }
+  console.log("enter lotto numGames", numGames)
+  buyLottoTicket(numGames);
+};
+
+ 
+  function truncate(str, maxDecimalDigits) {
+    if (str.includes('.')) {
+        const parts = str.split('.');
+        return parts[0] + '.' + parts[1].slice(0, maxDecimalDigits);
+    }
+    return str;
 }
 
-function truncate(str, maxDecimalDigits) {
-  if (str.includes('.')) {
-      const parts = str.split('.');
-      return parts[0] + '.' + parts[1].slice(0, maxDecimalDigits);
-  }
-  return str;
-}
+  // Function to randomly select numbers for each game
+  const generateRandomNumbers = () => {
+    const randomNumbers = Array.from({ length: numGames }, () => []);
+
+    for (let gameIndex = 0; gameIndex < numGames; gameIndex++) {
+      const availableNumbers = Array.from({ length: 30 }, (_, index) => index + 1);
+      for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+        const selectedNumber = availableNumbers.splice(randomIndex, 1)[0];
+        randomNumbers[gameIndex].push(selectedNumber);
+      }
+    }
+   console.log("numGAmes in random - ", numGames)
+    setSelectedNumbers(randomNumbers);
+  };
 
   return (
     
@@ -138,18 +258,7 @@ function truncate(str, maxDecimalDigits) {
   
   <div style={{ textAlign: 'center' }}>
   <h1>DGEN Lotto Draw #{ethSale.toString()}</h1>
-  <Image
-    src="/caeser.jpg"
-    alt="NFT"
-    width={250}
-    height={320}
-    style={{
-      display: 'block', // Make the image a block element for margin auto to work
-      margin: '0 auto', // Center align the image horizontally
-      borderRadius: '20%', // Make the border circular
-      border: '2px solid #ccc', // Add a border around the circular image
-    }}
-  />
+
 </div>
     <h1 className="second-h1" style={{ textAlign: 'center' }}>Live draw 8pm (Singapore timezone) Sunday 27 August. </h1>
     <h1 className="second-h1" style={{ textAlign: 'center' }}>Current Prizepool {truncate(ethers.utils.formatEther((contractBalance )), 4)} MATIC</h1>
@@ -172,7 +281,14 @@ function truncate(str, maxDecimalDigits) {
 </select>
       </div>
 
-
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <button
+          className="randomButton"
+          onClick={generateRandomNumbers}
+        >
+         Pick Numbers FOR ME
+        </button>
+      </div>
 
 
       {/* Generates the 30 numbers used to select from */}
@@ -211,10 +327,44 @@ function truncate(str, maxDecimalDigits) {
 
 
     {/* Submit button */}
-
+    <button
+      className={`submitButton ${isPulsing ? 'pulsing' : ''}`}
+      style={{
+        display: 'block',
+        margin: '0 auto',
+        padding: '10px 20px',
+        fontSize: '16px',
+        borderRadius: '5px',
+        border: 'none',
+        backgroundColor: '#0d6efd',
+        color: '#fff',
+        cursor: 'pointer',
+      }}
+      onClick={enterLotto}
+      onMouseOver={(e) => (e.target.style.backgroundColor = '#FFA500')}
+      onMouseOut={(e) => (e.target.style.backgroundColor = '#0d6efd')}
+    >
+      {submitButtonText}
+    </button>
   </div>
-
-
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+  {nftPurchased && nftImageUrl && (
+    <Image src={nftImageUrl} alt="NFT" width={350} height={500} />
+  )}
+</div>
+  {saleSucceeded && (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <a
+        href={txHash}
+        target="_blank"
+        rel="noreferrer"
+        className="underline underline-offset-2"
+        style={{ display: 'block' }}
+      >
+        View TX on block explorer
+      </a>
+    </div>
+  )}
   {/* CSS Styles */}
   <style>
     {`
